7aa54c8fb9c7bc9591b2cf732053b0c83c25b988
Juanky Soriano <juanky@juankysoriano.com>
2015-07-05 13:12:15 +0100

    Includes WorldRetrieverTest

---
+++ material-life-android/src/test/java/com/juankysoriano/materiallife/WorldRetrieverTest.java
@@ -0,0 +1,27 @@
+package com.juankysoriano.materiallife;
+
+import com.juankysoriano.materiallife.world.World;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class WorldRetrieverTest {
+    @Mock
+    private World world;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testWhenAWorldIsInjectedThenGetWorldReturnsThatWorld() {
+        WorldRetriever.INSTANCE.inject(world);
+
+        assertThat(WorldRetriever.INSTANCE.getWorld()).isEqualTo(world);
+    }
+}
