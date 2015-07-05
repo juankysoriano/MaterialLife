dda402cc7e6dbc3dd54c8d23d99be5c2cfaf2f36
Juanky Soriano <juanky@juankysoriano.com>
2015-07-05 13:12:31 +0100

    Includes ContextRetrieverTest

---
+++ material-life-android/src/test/java/com/juankysoriano/materiallife/ContextRetrieverTest.java
@@ -0,0 +1,56 @@
+package com.juankysoriano.materiallife;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class ContextRetrieverTest {
+
+    @Mock
+    private Application application;
+    @Mock
+    private Context applicationContext;
+    @Mock
+    private Activity activity;
+    @Mock
+    private Context activityContext;
+    @Mock
+    private Resources androidResources;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(application.getApplicationContext()).thenReturn(applicationContext);
+        when(application.getResources()).thenReturn(androidResources);
+    }
+
+    @Test
+    public void testThatWhenAnApplicationIsInjectedThenGetApplicationContextReturnsApplicationContext() {
+        ContextRetriever.INSTANCE.inject(application);
+
+        assertThat(ContextRetriever.INSTANCE.getApplicationContext()).isEqualTo(applicationContext);
+    }
+
+    @Test
+    public void testThatWhenAnApplicationIsInjectedThenGetResourcesReturnsApplicationResources() {
+        ContextRetriever.INSTANCE.inject(application);
+
+        assertThat(ContextRetriever.INSTANCE.getResources()).isEqualTo(androidResources);
+    }
+
+    @Test
+    public void testThatWhenAnActivityIsInjectedThenGetActivityReturnsInjectedActivity() {
+        ContextRetriever.INSTANCE.inject(activity);
+
+        assertThat(ContextRetriever.INSTANCE.getActivity()).isEqualTo(activity);
+    }
+}
