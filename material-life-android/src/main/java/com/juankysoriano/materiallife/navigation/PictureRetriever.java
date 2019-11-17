de5f1688fc7ff140e3833d95534f86545ef4ca10
Juanky Soriano <juanky@novoda.com>
2019-11-17 17:32:55 -0600

    Fix retrieving world from camera

--- material-life-android/src/main/java/com/juankysoriano/materiallife/navigation/PictureRetriever.java
+++ material-life-android/src/main/java/com/juankysoriano/materiallife/navigation/PictureRetriever.java
@@ -6,16 +6,20 @@ import android.os.Environment;
 import android.provider.MediaStore;
 
 import com.juankysoriano.materiallife.ContextRetriever;
+import com.juankysoriano.materiallife.R;
 import com.juankysoriano.materiallife.imageloader.ImageLoaderResult;
 import com.novoda.notils.logger.simple.Log;
 
 import java.io.File;
 import java.io.IOException;
 
+import androidx.core.content.FileProvider;
+
 public class PictureRetriever {
     private static final String MATERIAL_LIFE = "MATERIAL_LIFE";
     private static final String EXTENSION = ".jpg";
     private static final String FILE_SCHEME = "file:";
+
     private String lastLoadedImagePath;
 
     public void openCameraForResult() {
@@ -36,21 +40,25 @@ public class PictureRetriever {
     private Intent createIntentForCamera() {
         try {
             return new Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    .putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(createImageFile()));
+                    .putExtra(MediaStore.EXTRA_OUTPUT, createImageFile());
         } catch (IOException e) {
             Log.e("Problem creating picture:" + e);
         }
         return null;
     }
 
-    private Intent createIntentForGallery() {
-        return new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-    }
-
-    private File createImageFile() throws IOException {
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+    private Uri createImageFile() throws IOException {
+        File storageDir = ContextRetriever.INSTANCE.getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(MATERIAL_LIFE, EXTENSION, storageDir);
         lastLoadedImagePath = FILE_SCHEME + image.getAbsolutePath();
-        return image;
+        return FileProvider.getUriForFile(
+                ContextRetriever.INSTANCE.getApplicationContext(),
+                ContextRetriever.INSTANCE.getResources().getString(R.string.file_provider),
+                image
+        );
+    }
+
+    private Intent createIntentForGallery() {
+        return new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
     }
 }
