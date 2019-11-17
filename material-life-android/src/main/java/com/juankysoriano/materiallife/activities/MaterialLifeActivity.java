993692c58cc679de2fe9fa1cb9776b981886b77d
Juanky Soriano <juanky@novoda.com>
2019-11-17 17:09:57 -0600

    Remove analytics

--- material-life-android/src/main/java/com/juankysoriano/materiallife/activities/MaterialLifeActivity.java
+++ material-life-android/src/main/java/com/juankysoriano/materiallife/activities/MaterialLifeActivity.java
@@ -1,10 +1,10 @@
 package com.juankysoriano.materiallife.activities;
 
 import android.os.Bundle;
-import androidx.fragment.app.FragmentActivity;
 
 import com.juankysoriano.materiallife.ContextRetriever;
-import com.juankysoriano.materiallife.analytics.AnalyticsTracker;
+
+import androidx.fragment.app.FragmentActivity;
 
 public class MaterialLifeActivity extends FragmentActivity {
 
@@ -12,18 +12,6 @@ public class MaterialLifeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ContextRetriever.INSTANCE.inject(this);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        AnalyticsTracker.INSTANCE.trackActivityStart(this);
-    }
-
-    @Override
-    protected void onStop() {
-        AnalyticsTracker.INSTANCE.trackActivityStop(this);
-        super.onStop();
     }
 
     @Override
